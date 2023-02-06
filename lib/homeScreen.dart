b014163f784f00bc1e1e415e900f7d711a167b13
theAkHilsarkar18 <akhilsodvadiya7@gmail.com>
2023-02-06 13:09:23 +0530

    06-02-2023

--- lib/homeScreen.dart
+++ lib/homeScreen.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -13,31 +15,89 @@ class _FontImgState extends State<FontImg> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        backgroundColor: Colors.white54,
-        body: Column(
+        backgroundColor: Colors.white,
+        body: Stack(
           children: [
             Container(
-              margin: EdgeInsets.all(20),
-              height: 100,
-              width: double.infinity,
-              alignment: Alignment.centerLeft,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text("/63",style: TextStyle(color: Colors.grey.shade600,fontSize: 18,fontWeight: FontWeight.bold),),
-                  Row(
-                    children: [
-                      Text("ATLANTIC",style: GoogleFonts.mukta(textStyle: TextStyle(fontSize: 19,color: Colors.grey.shade600,fontWeight: FontWeight.bold,letterSpacing: 1))),
-                      Spacer(),
-                      Text("Gallery",style: TextStyle(color: Colors.grey.shade600)),
-                    ],
-                  ),
-                ],
-              ),
-            ),
+                height: double.infinity,
+                width: double.infinity,
+                child: Image.asset(
+                  "assets/image/fontimg.jpeg",
+                  fit: BoxFit.fill,
+                )),
             Column(
               children: [
-                
+                Container(
+                  margin: EdgeInsets.all(20),
+                  height: 100,
+                  width: double.infinity,
+                  alignment: Alignment.centerLeft,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "/63",
+                        style: TextStyle(
+                            color: Colors.grey.shade600,
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      Row(
+                        children: [
+                          Text("ATLANTIC",
+                              style: GoogleFonts.mukta(
+                                  textStyle: TextStyle(
+                                      fontSize: 19,
+                                      color: Colors.grey.shade600,
+                                      fontWeight: FontWeight.bold,
+                                      letterSpacing: 1))),
+                          Spacer(),
+                          Text("Gallery",
+                              style: TextStyle(color: Colors.grey.shade600)),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                Transform.rotate(
+                  angle: pi / 2,
+                  child: Align(
+                    alignment: Alignment.center,
+                    child: Transform.translate(
+                      offset: Offset(10,130),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Akhil Sarkar",
+                            style: GoogleFonts.rubik(
+                                fontSize: 18,
+                                color: Colors.grey.shade600,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            "Capital",
+                            style: GoogleFonts.rubik(
+                              fontSize: 15,
+                              color: Colors.grey.shade600,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                Transform.translate(
+                  offset: Offset(100,450),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text("Type of Ratina i",style: GoogleFonts.rubik(color: Colors.grey.shade600,fontWeight: FontWeight.bold)),
+                      Text("1947 to 2023",style: GoogleFonts.rubik(color: Colors.grey.shade600, fontSize: 9)),
+                    ],
+                  ),
+                ),
+                Transform.translate(offset: Offset(100,480),child: Icon(Icons.menu,color: Colors.grey.shade600,)),
               ],
             ),
           ],
